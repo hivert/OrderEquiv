229efed148afdb4c20c764c63f6a84d84d23bf42
Florent Hivert <Florent.Hivert@lri.fr>
2024-01-08 04:21:10 +0100

    More Cleanup

--- theories/equiv.v
+++ theories/equiv.v
@@ -26,8 +26,6 @@ Open Scope order_scope.
 
 
 
-Definition bool_simp := (addbF, addbT, andbT, andbF, orbT, orbF).
-
 Lemma predext (T : Type) (p1 p2 : simpl_pred T) : p1 = p2 <-> p1 =i p2.
 Proof.
 split => [-> x // | eqf].
@@ -36,16 +34,20 @@ by apply funext => x; have:= eqf x; rewrite !unfold_in /=.
 Qed.
 
 
-
 Section CanonicalEnumeration.
 
 Lemma enum_bool : enum (bool : finType) = [:: true; false].
 Proof. by rewrite enumT unlock. Qed.
 
+Lemma enum_prod_bool_T (T : finType) :
+  enum ((bool * T)%type : finType) =
+  [seq (true, t) | t <- enum T] ++ [seq (false, t) | t <- enum T].
+Proof. by rewrite enumT unlock /= /prod_enum enum_bool /= cats0. Qed.
+
 Lemma enum_bool_bool :
   enum ((bool * bool)%type : finType) =
   [:: (true, true); (true, false); (false, true); (false, false) ].
-Proof. by rewrite enumT unlock /= /prod_enum enum_bool /=. Qed.
+Proof. by rewrite enum_prod_bool_T enum_bool. Qed.
 
 Lemma eq_finset_enum (T : finType) (s1 s2 : {set T}) :
   (s1 == s2) = all (fun pr => (pr \in s1) == (pr \in s2)) (enum T).
@@ -59,7 +61,7 @@ End CanonicalEnumeration.
 
 Section Def.
 
-Variable (u : unit) (Ord : orderType u).
+Context {u : unit} (Ord : orderType u).
 
 Implicit Type (P : pred Ord).
 
@@ -106,7 +108,7 @@ Definition char n P := nosimpl (char_rec n P).
 Lemma char0P P : reflect (exists x, x \in P) (char 0 P).
 Proof. exact: (iffP (asboolP _)). Qed.
 
-Lemma mem_charP {n} P l r :
+Lemma mem_charP n P l r :
   reflect
     (exists x, [/\ x \in P, char n (down P x) = l & char n (up P x) = r])
     ((l, r) \in char n.+1 P).
@@ -116,23 +118,21 @@ apply (iffP idP); rewrite /char /= inE.
 - move=> [x [xinP <- <-]]; apply/asboolP; exists x.
   exact/andP.
 Qed.
+Arguments mem_charP (n P l r) : clear implicits.
 
 Lemma char0_pred0 : char 0 pred0 = false.
 Proof. by apply/char0P => [] [x]; rewrite inE. Qed.
 Lemma char0_true P : char 0 P -> P <> pred0.
-Proof.
-move/char0P => [x xinP eqP0].
-by move: xinP; rewrite eqP0 inE.
-Qed.
+Proof. by move/char0P => [x /[swap] ->]; rewrite inE. Qed.
 
 Lemma char_pred0 n : char n.+1 pred0 = set0.
 Proof.
 apply/setP => [[l r]]; rewrite [RHS]inE.
-apply/negP => /(@mem_charP n) [x []].
+apply/negP => /mem_charP [x []].
 by rewrite inE.
 Qed.
 
-(* James's 6-th lemma *)
+(** James's 6-th lemma *)
 Lemma char1_eq0P P : char 1 P = set0 -> P = pred0.
 Proof.
 move=> ch1; apply funext => x /=.
@@ -145,22 +145,22 @@ Qed.
 Lemma char1_pred1 x0 : char 1 (pred1 x0) = [set (false, false)].
 Proof.
 apply/setP => [] [[|] /= b]; rewrite in_set1 ?eqE /=.
-  apply/negP => /(@mem_charP 0) /= [x []].
+  apply/negP => /(mem_charP 0) /= [x []].
   rewrite inE => /eqP ->{x}.
   by rewrite down_pred1 char0_pred0.
 case: b; rewrite eqE /= /eqb /=.
-- apply/negP => /(@mem_charP 0) /= [x []].
+- apply/negP => /(mem_charP 0) /= [x []].
   rewrite inE => /eqP ->{x} _.
   by rewrite up_pred1 char0_pred0.
-- apply/(@mem_charP 0); exists x0.
+- apply/(mem_charP 0); exists x0.
   by rewrite inE down_pred1 up_pred1 char0_pred0.
 Qed.
 
-(* James's 7-th lemma *)
+(** James's 7-th lemma *)
 Lemma charFF P :
   (false, false) \in char 1 P -> exists x, P = pred1 x.
 Proof.
-move/(@mem_charP 0) => [x [xinP /char0P downP /char0P upP]].
+move/(mem_charP 0) => [x [xinP /char0P downP /char0P upP]].
 exists x; apply funext => y.
 rewrite [RHS]inE /= eq_sym.
 have /= -> := (topredE y P).
@@ -179,18 +179,18 @@ Lemma up_down P x y :
   x \in P -> y \in P -> (y \in up P x) = (x \in down P y).
 Proof. by rewrite !inE => -> ->. Qed.
 
-(* James's 5-th lemma *)
+(** James's 5-th lemma *)
 Lemma charFT P :
   (false, true) \in char 1 P ->
   (true, true) \in char 1 P \/ (true, false) \in char 1 P.
 Proof.
-move/(@mem_charP 0) => [x0 [x0inP /char0P downP /char0P [x1 /upP[x1inP lt01]]]].
+move/(mem_charP 0) => [x0 [x0inP /char0P downP /char0P [x1 /upP[x1inP lt01]]]].
 case: (boolP ((true, true) \in char 1 P)) => Htt; [left | right] => //.
-apply/(@mem_charP 0); exists x1; split; first by [].
+apply/(mem_charP 0); exists x1; split; first by [].
 - apply/char0P; exists x0.
   by rewrite -up_down //; apply/upP.
 - apply/char0P => [] [x2 /upP [x2inP lt12]].
-  move: Htt => /negP; apply; apply/(@mem_charP 0).
+  move: Htt => /negP; apply; apply/(mem_charP 0).
   exists x1; split => //; apply/char0P.
   + by exists x0; apply/upP.
   + by exists x2; apply/upP.
@@ -204,13 +204,9 @@ Section Dual.
 Variable (u : unit) (Ord : orderType u).
 Implicit Type (x t : Ord) (P : pred Ord).
 
-Local Arguments char {u} Ord.
-Local Arguments down {u} Ord.
-Local Arguments up {u} Ord.
-
-Lemma down_dual P x : down Ord^d P x = up Ord P x.
+Lemma down_dual P x : down (Ord := Ord^d) P x = up P x.
 Proof. by apply predext => y; rewrite !inE. Qed.
-Lemma up_dual P x : up Ord^d P x = down Ord P x.
+Lemma up_dual P x : up (Ord := Ord^d) P x = down P x.
 Proof. by apply predext => y; rewrite !inE. Qed.
 
 
@@ -241,10 +237,11 @@ Lemma mem_rev_char1 (ch1 ch2 : bool) (ch : char_type 1) :
   ((ch1, ch2) \in rev_char ch) = ((ch2, ch1) \in ch).
 Proof. by rewrite (@mem_rev_char 0). Qed.
 
-Lemma char0_dual P : char Ord 0 P = char Ord^d 0 P.
+Lemma char0_dual P : char (Ord := Ord) 0 P = char (Ord := Ord^d) 0 P.
 Proof. by apply/char0P/char0P => [] [x xinP]; exists x. Qed.
 
-Lemma char_dual n P : rev_char (char Ord^d n P) = char Ord n P.
+Lemma char_dual n P :
+  rev_char (char (Ord := Ord^d) n P) = char (Ord := Ord) n P.
 Proof.
 elim: n P => [|n IHn] P /=; first exact: char0_dual.
 apply/setP => /= [[chdown chup]].
@@ -270,6 +267,8 @@ Lemma charTF P :
   (true, true) \in char 1 P \/ (false, true) \in char 1 P.
 Proof. by rewrite -(char_dual 1 P) !mem_rev_char1; apply: charFT. Qed.
 
+
+(** This is a characterization of sets which are of char 1 of some predicate *)
 Definition is_char1 :=
   [pred ch : char_type 1 | [&&
    (((false, false) \in ch) ==> (ch == [set (false, false)])),
@@ -291,8 +290,8 @@ End CharPredicate.
 
 
 
-(*******************************************)
-(* Various examples for the classification *)
+(********************************************)
+(** Various examples for the classification *)
 
 Lemma char0_nat : char 0 (@predT nat) = true.
 Proof. by apply/char0P; exists 0. Qed.
@@ -412,11 +411,13 @@ apply/setP => [] [[|] [|]]; rewrite ![in RHS]inE ?eqE /= ?eqE /= ?/eqb /=.
 Qed.
 
 
+(** The [char 1] classification *)
 Section Classification.
 
 Variables (u : unit) (Ord : orderType u).
 Implicit Type (x t : Ord) (P : pred Ord) (ch : char_type 1).
 
+(** Predicate describing a subset of an ordered set *)
 Record ordPred := OrdPred {
                       disp_ordPred : unit;
                       type_ordPred : orderType disp_ordPred;
@@ -432,12 +433,13 @@ Definition classif1_order : seq ordPred := [::
      OrdPred (@predT nat^d);        (* -N        *)
      OrdPred (@predT int)           (* Z         *)
   ].
-Definition classif1 := map (fun pr : ordPred => char 1 pr) classif1_order.
+Definition classif1 :=
+  [tuple of map (fun pr : ordPred => char 1 pr) classif1_order].
 Definition classif1E := (char_pred0 nat, char1_pred1 0%N,
                 char1_down2, char1_down3, char1_nat, char1_nat_dual, char1_int).
 
-(* There are no duplicate in the classification *)
-Proposition classif_uniq : uniq classif1.
+(** There are no duplicate in the classification *)
+Proposition classif1_uniq : uniq classif1.
 Proof.
 rewrite /classif1 /classif1_order /= !classif1E /=.
 by rewrite !inE !eq_finset_enum !enum_bool_bool /= !inE.
@@ -446,18 +448,23 @@ Qed.
 Theorem is_char1E ch : (is_char1 ch) = (ch \in classif1).
 Proof.
 apply/idP/idP.
-- (* Expand the definitions and the classification *)
+- (** Expand the definitions and the classification *)
   rewrite /classif1 /classif1_order /= !classif1E /=.
-  (* Expand computationally equality of char 1 *)
+  (** Expand computationally equality of char 1 *)
   rewrite !inE !eq_finset_enum !enum_bool_bool /= !inE.
-  (* case analysis *)
+  (** case analysis *)
   by repeat case: (boolP (_ \in _)) => _; rewrite ?inE.
 - rewrite !inE.
   by repeat move/orP => []; try (move/eqP->; exact: is_char1P).
 Qed.
 
-(* All characters are in the classification *)
-Corollary classifP P : char 1 P \in classif1.
-Proof. rewrite -is_char1E; exact: is_char1P. Qed.
+(** All characters are in the classification *)
+Corollary classifP P : exists! i : 'I_7, char 1 P = tnth classif1 i.
+Proof.
+have /tnthP [i Hi] : char 1 P \in classif1.
+  by rewrite -is_char1E; exact: is_char1P.
+exists i; split => // j; rewrite {}Hi.
+exact/tuple_uniqP/classif1_uniq.
+Qed.
 
 End Classification.
